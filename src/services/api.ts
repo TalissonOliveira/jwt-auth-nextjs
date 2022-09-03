85e9275fa9472a6ef3262b7f61b0fc6021391649
Talisson Oliveira <talissonlux@outlook.com>
2022-09-03 00:51:07 -0300

    feat: creates interceptor in axios to refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,48 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+interface AxiosErrorResponse {
+  code?: string;
+}
+
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333/',
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError<AxiosErrorResponse>) => {
+  console.log(error.response)
+  if (error.response?.status === 401) {
+    if (error.response?.data?.code === 'token.expired') {
+      cookies = parseCookies()
+
+      const { 'nextauth.refreshToken': refreshToken } = cookies
+
+      api.post('/refresh', {
+        refreshToken
+      }).then(response => {
+        const { token } = response.data
+
+        setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      })
+    } else {
+
+    }
   }
 })
